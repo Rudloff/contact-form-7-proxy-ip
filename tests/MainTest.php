480da5ba3ef1bd17e22182b471b8169ec96d49cb
Pierre Rudloff <contact@rudloff.pro>
2017-01-16 18:43:48 +0100

    Unused properties

--- tests/MainTest.php
+++ tests/MainTest.php
@@ -26,33 +26,33 @@ class MainTest extends \PHPUnit_Framework_TestCase
     public function testAddSpecialTagsWithWrongTag()
     {
         $this->submissionMock->shouldReceive('get_instance')->andReturn(new \WPCF7_Submission());
-        $this->assertEmpty(Main::addSpecialTags('', '_remote_ip', false));
+        $this->assertEmpty(Main::addSpecialTags('', '_remote_ip'));
     }
 
     public function testAddSpecialTagsWithoutSubmission()
     {
         $this->submissionMock->shouldReceive('get_instance');
-        $this->assertEmpty(Main::addSpecialTags('', '_forwarded_ip', false));
+        $this->assertEmpty(Main::addSpecialTags('', '_forwarded_ip'));
     }
 
     public function testAddSpecialTagsWithoutRemoteIp()
     {
         $this->submissionMock->shouldReceive('get_meta');
         $this->submissionMock->shouldReceive('get_instance')->andReturn(new \WPCF7_Submission());
-        $this->assertEmpty(Main::addSpecialTags('', '_forwarded_ip', false));
+        $this->assertEmpty(Main::addSpecialTags('', '_forwarded_ip'));
     }
 
     public function testAddSpecialTagsWithRemoteIp()
     {
         $this->submissionMock->shouldReceive('get_meta')->andReturn('127.0.0.1');
         $this->submissionMock->shouldReceive('get_instance')->andReturn(new \WPCF7_Submission());
-        $this->assertEquals('127.0.0.1', Main::addSpecialTags('', '_forwarded_ip', false));
+        $this->assertEquals('127.0.0.1', Main::addSpecialTags('', '_forwarded_ip'));
     }
 
     public function testAddSpecialTagsWithForwardedIp()
     {
         $_SERVER['HTTP_X_FORWARDED_FOR'] = '127.0.0.1';
         $this->submissionMock->shouldReceive('get_instance')->andReturn(new \WPCF7_Submission());
-        $this->assertEquals('127.0.0.1', Main::addSpecialTags('', '_forwarded_ip', false));
+        $this->assertEquals('127.0.0.1', Main::addSpecialTags('', '_forwarded_ip'));
     }
 }
